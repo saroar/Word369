8b5736941f60013fdaff87b1d496f3ad79e90d42
saroar <saroar9@gmail.com>
2021-11-22 10:37:05 +0300

    Initial Commit

---
+++ Word369/Word369App.swift
@@ -0,0 +1,17 @@
+//
+//  Word369App.swift
+//  Word369
+//
+//  Created by Saroar Khandoker on 22.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct Word369App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
